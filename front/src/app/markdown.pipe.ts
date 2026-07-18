8b4bf923ffcd7b2163178230a2bcfa9c570691b9
Yar-Ner <48760654+Yar-Ner@users.noreply.github.com>
2026-07-19 00:26:57 +0300

    Update markdown.pipe.ts

--- front/src/app/markdown.pipe.ts
+++ front/src/app/markdown.pipe.ts
@@ -8,7 +8,6 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class MarkdownPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
-
   transform(value: string): SafeHtml {
     if (!value) return '';
     return this.sanitizer.bypassSecurityTrustHtml(marked.parse(value) as string);
